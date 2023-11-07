ea45d5f779d6bf6b47c673e4206c6cfac827c86e
Phuong Trang <tnguyenhuynhphuong@gmail.com>
2023-11-07 11:51:46 +0700

    react hook form

---
+++ src/components/CreateStaff.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { useForm } from 'react-hook-form'
+import {yupResolver} from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup.object({
+    fullname: yup.string().required("Bạn cần cung cấp họ và tên"),
+    age: yup.number()
+        .min(18, "Tuổi phải lớn hơn hoặc bằng 18")
+        .max(30)
+        .required()
+        .typeError("Bạn cần phải cung cấp tuổi"),
+    gender: yup.string().required(),
+    city: yup.string().required(),
+    email: yup.string().required().email(),
+    password: yup.string().length(8).required(),
+    confirmPassword: yup.string()
+        .required()
+        .oneOf([yup.ref('password')], "confirmPassword does not match"),
+    dob: yup.date()
+        .required()
+        .typeError('dob is a required field')
+})
+
+const cityList = [
+    {
+        id: 1,
+        name: "TT Huế"
+    },
+    {
+        id: 2,
+        name: "Đà Nẵng"
+    },
+    {
+        id: 3,
+        name: "Quảng Trị"
+    },
+    {
+        id: 4,
+        name: "Hồ Chí Minh"
+    }
+]
+function CreateStaff(){
+
+    const {register, handleSubmit, formState: {errors}, reset} = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+        criteriaMode: "all"
+    })
+
+    const handleCreateStaff = (data) => {
+        console.log(data);
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit(handleCreateStaff)} className="needs-validation">
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Full Name</label>
+                    <input type="text" 
+                        className={`form-control ${errors?.fullname?.message ? 'is-invalid' : ''}`}
+                        {...register('fullname')}
+                    />
+                    <span className="invalid-feedback">{errors?.fullname?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Email</label>
+                    <input type="text" 
+                        className={`form-control ${errors?.email?.message ? 'is-invalid' : ''}`}
+                        {...register('email')}
+                    />
+                    <span className="invalid-feedback">{errors?.email?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Password</label>
+                    <input type="password" 
+                        className={`form-control ${errors?.password?.message ? 'is-invalid' : ''}`}
+                        {...register('password')}
+                    />
+                    <span className="invalid-feedback">{errors?.email?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Confirm Password</label>
+                    <input type="password" 
+                        className={`form-control ${errors?.confirmPassword?.message ? 'is-invalid' : ''}`}
+                        {...register('confirmPassword')}
+                    />
+                    <span className="invalid-feedback">{errors?.confirmPassword?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Date of Birth</label>
+                    <input type="date"
+                        className={`form-control ${errors?.dob?.message ? 'is-invalid' : ''}`}
+                        {...register('dob')}
+                    />
+                    <span className="invalid-feedback">
+                        {errors?.dob?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Age</label>
+                    <input type="number"
+                        className={`form-control ${errors?.age?.message ? 'is-invalid' : ''}`}
+                        {...register('age')}
+                    />
+                    <span className="invalid-feedback">
+                        {errors?.age?.message}</span>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">City</label>
+                    <select 
+                        className={`form-control ${errors?.city?.message ? 'is-invalid' : ''}`}
+                        {...register('city')}>
+                        <option value={''}>Please choose a City</option>
+                        {
+                            cityList.map((city) => (
+                                <option value={city.name} key={city.id}>{city.name}</option>
+                            ))
+                        }
+                    </select>
+                </div>
+                <div className="form-group mb-3 has-validation">
+                    <label className="form-label">Gender</label>
+                    <div>
+                        <div className="form-check form-check-inline">
+                            <input
+                                className={`form-check-input ${errors?.gender?.message ? 'is-invalid' : ''}`}
+                                type="radio" value="male"
+                                {...register('gender')}
+                            />
+                            <label className="form-check-label">Male</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input 
+                            className={`form-check-input ${errors?.gender?.message ? 'is-invalid' : ''}`}
+                            type="radio" value="female"
+                                {...register('gender')}
+                            />
+                            <label className="form-check-label">Female</label>
+                        </div>
+                        <span className="invalid-feedback d-block">{errors?.gender?.message}</span>
+                    </div>
+                </div>
+
+                <div className="form-group mb-3">
+                    <button type="submit" className="btn btn-success me-3">Create</button>
+                    <button type="button" className="btn btn-dark"
+                        onClick={() => reset()}
+                    >Cancel</button>
+                </div>
+            </form>
+    </div>
+    )
+}
+
+export default CreateStaff;
