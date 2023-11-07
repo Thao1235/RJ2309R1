ad006678590dc8134dcff71bb156528a8d1669cd
Phuong Trang <tnguyenhuynhphuong@gmail.com>
2023-11-07 18:44:35 +0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Components/Home/HomePage';
+import StudentList from './Components/Student/StudentList';
+import CreateStudent from './Components/Student/CreateStudent';
+import Navbar from './Components/Layout/Navbar';
+import StudentDetail from './Components/Student/StudentDetail';
+import NotFound from './Components/Layout/NotFound';
+
 
 function App() {
   return (
-    <div className="container">
-      
-    </div>
+    <>
+      <Navbar/>
+      <div className='container'>
+        <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/Home' element={<HomePage/>}/>
+            <Route path='/student' element={<StudentList/>}/>
+            <Route path='/student/add' element={<CreateStudent/>}/>
+            <Route path='/student/detail/:studentId/:name' element={<StudentDetail/>}/>
+            <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </div>
+    </>
+    
   );
 }
 
